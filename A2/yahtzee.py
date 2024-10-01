87f32e20f1bc13643df41b1b389483bedc5f1ae0
hyzoon <shj83811081@gmail.com>
2024-10-01 18:17:28 +0900

    feat: initial code for assg 2

---
+++ A2/yahtzee.py
@@ -0,0 +1,23 @@
+# For random number generation
+import random
+
+# Function to roll 5 dice except for the ones that are kept
+def roll_dices(kept_dices):
+    return 
+
+
+# Function to calculate the score of a given dice combination and print the score sheet
+def calculate_score(dice_combination):
+    return
+
+# Function to choose the unrecorded category and record the score
+def record_score(score_sheet):
+    return
+
+# Function to check the current score sheet
+def check_score(score_sheet):
+    return
+
+# Function to play the game
+def game_start():
+    return
